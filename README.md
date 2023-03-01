0645738cf5bc4bd7ec5b6170bad4d1d28f29c729
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2023-03-01 15:38:59 -0500

    Add logs and usage

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Images With Faces Filter
 
 This repo contains a script that will take a given set of images, located in the pre_filter folder, and populate the post_filter folder with only images from that original set that contain faces.
+
+## Usage
+
+Follow the instructions to install the `face_recognition` library [here](https://github.com/ageitgey/face_recognition#installation), then run `python main.py`.
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+from PIL import Image
 import face_recognition
 import os
 
-PRE_FILTER_DIR_PATH = "/pre_filter"
-POST_FILTER_DIR_PATH = "/post_filter"
+PRE_FILTER_DIR_PATH = "pre_filter/"
+POST_FILTER_DIR_PATH = "post_filter/"
 
+# Create post_filter folder if it doesn't already exist
+if not (os.path.exists(POST_FILTER_DIR_PATH)):
+    os.mkdir(POST_FILTER_DIR_PATH)
+
+# Save each image in pre_filter folder to post_filter folder if a face is detected
+num_images_saved = 0
 for filename in os.listdir(PRE_FILTER_DIR_PATH):
-    image = face_recognition.load_image_file(PRE_FILTER_DIR_PATH + "/" + filename)
-    
+
+    image = face_recognition.load_image_file(PRE_FILTER_DIR_PATH + filename)
     face_locations = face_recognition.face_locations(image)
+
     if len(face_locations) > 0:
-        image.save(POST_FILTER_DIR_PATH + "/" + filename)
+        print("Face detected, saving image " + filename)
+        image = Image.fromarray(image)
+        image.save(POST_FILTER_DIR_PATH + filename)
+        num_images_saved += 1
+
+print("Saved " + str(num_images_saved) + " images in total")
